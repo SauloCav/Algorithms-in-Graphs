c8220c3f366e68f945174397351c354a3704dce2
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-05-08 09:02:55 -0300

    Update CycleDetector.py

--- Cycle Detector/CycleDetector.py
+++ Cycle Detector/CycleDetector.py
@@ -1,47 +1,39 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#O objetivo basico é encontrar uma aresta de retorno para algum nó já visitado. 
-
 class Graph:
  
     def __init__(self, edges, numNodes):
  
-        self.adjacencyList = [[] for _ in range(numNodes)] #É criada uma lista de adjacências com o número de nós passado
-
-        #Cada um dos vertices passados são adicionados na lista
+        self.adjacencyList = [[] for _ in range(numNodes)]
+  
         for (source, destination) in edges:
             self.adjacencyList[source].append(destination)
             self.adjacencyList[destination].append(source)
 
-def DFS_Traversal(graph, v, visited, parent_node =- 1): #Nó parente trata-se no nó anteriormente analizado
+def DFS_Traversal(graph, v, visited, parent_node =- 1):
  
-    visited[v] = True #Marca o nó atual como true
+    visited[v] = True
 
-    for u in graph.adjacencyList[v]: #Para cada vértice é feita a análise
-        if not visited[u]: #Se u não é visitado é feita uma re-análise
+    for u in graph.adjacencyList[v]:
+        if not visited[u]:
             if DFS_Traversal(graph, u, visited, v):
                 return True
-        elif u != parent_node: #Se u for visitado e não é um nó parente então há um ciclo
+        elif u != parent_node:
             return True
 
     return False
  
 if __name__ == '__main__':
     
-    #Com ciclo
-    edges = [ (0, 1), (0, 2), (2, 3), (2, 4), (2, 1) ] #Definição dos vertices
-
-    #Sem ciclo
-    #edges = [ (0, 1), (1, 3), (1, 4), (2, 4)] #Definição dos vertices
+    edges = [ (0, 1), (0, 2), (2, 3), (2, 4), (2, 1) ]
  
-    numNodes = 5 #Numero de nós do grafo
+    numNodes = 5 
 
-    constructed_graph = Graph(edges, numNodes) #Os vértices e o número de nós são então enviados para a montagem do grafo
+    constructed_graph = Graph(edges, numNodes) 
 
-    visited = [False] * numNodes #Passa os nós como não visitados
-
-    #DFS_Traversal é chamada
+    visited = [False] * numNodes 
+    
     if DFS_Traversal(constructed_graph, 0, visited):
         print('Um Ciclo Detectado!')
     else:
