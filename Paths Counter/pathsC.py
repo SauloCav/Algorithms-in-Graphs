5e665cd3c4840310982521661d1fbf6241491658
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-06-09 07:12:10 -0300

    Update pathsC.py

--- Paths Counter/pathsC.py
+++ Paths Counter/pathsC.py
@@ -1,6 +1,3 @@
-#Python program
-#Count all possible paths between two vertices
-#Using DFS
 class AjLinkeNode:
   def __init__(self,data):
     self.id=data
@@ -17,7 +14,6 @@ class Graph:
     self.size=size
     self.node=[]
     self.result=0
-    
 
   def setData(self):
     if(self.size>0 and self.node!=None):
@@ -29,8 +25,6 @@ class Graph:
 
         index+=1
 
-
-  #connect two node with  edge
   def connect(self,start,end):
 
     new_edge=AjLinkeNode(end)
@@ -80,51 +74,38 @@ class Graph:
 
         index+=1
 
-  
-  #This function are capable to count all path between two nodes
   def count_path(self,start,end,visit) :
 
     if(start >self.size  or  end >self.size or start<0 or end<0 or self.node==None) :
 
-      #invalid node location
       return 
     if(visit[start]==True) :
 
-      #node is already visited
       return
     if(start==end) :
 
-      #when source and destination are get
       self.result+=1
 
-    #active the visiting node status
     visit[start]=True
     temp=self.node[start].next
     while(temp!=None) :
 
-      #check node by edges
       self.count_path(temp.id,end,visit) 
       temp=temp.next
 
-    #inactive current visiting node status
     visit[start]=False
 
   def  total_path(self,source,destination) :
 
-    #for resultant paths
     self.result=0
     visit=[False]*self.size
 
     self.count_path(source,destination,visit)
     print("\n Possible Path : ",self.result)
     
-
-
-    
 def main():
   g=Graph(6)
   g.setData() 
-  #Connected two node with Edges
   g.addEdge(0, 1) 
   g.addEdge(0, 3) 
   g.addEdge(1, 2) 
@@ -136,9 +117,8 @@ def main():
   g.addEdge(3, 5) 
   g.addEdge(5, 4) 
   g.printGraph()
-  #value 0 is source node
-  source = 0
-  #value 4 is destination node
+
+
   destination = 4
   g.total_path(source,destination)
 
