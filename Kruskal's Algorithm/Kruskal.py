49ae030df642f513cf13bb19ee43d792b99d132d
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2022-06-11 17:09:50 -0300

    adiciona ciclo negativo em kruskal

--- Kruskal's Algorithm/Kruskal.py
+++ Kruskal's Algorithm/Kruskal.py
@@ -79,4 +79,10 @@ g.addEdge(0, 2, 10)
 g.addEdge(0, 3, 2)
 g.addEdge(1, 3, 2)
 
+#Grafo com ciclo negativo
+#g = Graph(3)
+#g.addEdge(0, 1, 1)
+#g.addEdge(0, 2, 2)
+#g.addEdge(1, 0, -2)
+
 g.KruskalMST()
