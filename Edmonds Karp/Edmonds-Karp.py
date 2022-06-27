d36956cf2b61e525e8d1e46d48635b68b5cb86a5
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-06-27 10:48:16 -0300

    Update Edmonds-Karp.py

--- Edmonds Karp/Edmonds-Karp.py
+++ Edmonds Karp/Edmonds-Karp.py
@@ -1,9 +1,8 @@
 
 def max_flow(C, s, t):
-        n = len(C) # C is the capacity matrix
+        n = len(C)
         F = [[0] * n for i in xrange(n)]
         path = bfs(C, F, s, t)
-      #  print path
         while path != None:
             flow = min(C[u][v] - F[u][v] for u,v in path)
             for u,v in path:
@@ -11,7 +10,6 @@ def max_flow(C, s, t):
                 F[v][u] -= flow
             path = bfs(C, F, s, t)
         return sum(F[s][i] for i in xrange(n))
-
 
 def bfs(C, F, s, t):
         queue = [s]
@@ -29,16 +27,15 @@ def bfs(C, F, s, t):
                         queue.append(v)
         return None
     
-C = [[ 0, 3, 3, 0, 0, 0 ],  # s
-     [ 0, 0, 2, 3, 0, 0 ],  # o
-     [ 0, 0, 0, 0, 2, 0 ],  # p
-     [ 0, 0, 0, 0, 4, 2 ],  # q
-     [ 0, 0, 0, 0, 0, 2 ],  # r
-     [ 0, 0, 0, 0, 0, 3 ]]  # t
+C = [[ 0, 3, 3, 0, 0, 0 ],
+     [ 0, 0, 2, 3, 0, 0 ],
+     [ 0, 0, 0, 0, 2, 0 ],
+     [ 0, 0, 0, 0, 4, 2 ],
+     [ 0, 0, 0, 0, 0, 2 ],
+     [ 0, 0, 0, 0, 0, 3 ]]
 
-source = 0  # A
-sink = 5    # F
+source = 0 
+sink = 5 
 max_flow_value = max_flow(C, source, sink)
 print "Edmonds-Karp algorithm"
 print "max_flow_value is: ", max_flow_value
-
