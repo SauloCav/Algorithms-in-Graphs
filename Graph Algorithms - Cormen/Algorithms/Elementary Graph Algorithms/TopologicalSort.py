0ac621190754f4e38b8295f06f5681fd0beb6d74
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2024-02-05 13:51:39 -0300

    Create TopologicalSort.py

---
+++ Graph Algorithms - Cormen/Algorithms/Elementary Graph Algorithms/TopologicalSort.py
@@ -0,0 +1,4 @@
+TOPOLOGICAL-SORT.G/
+1 call DFS.G/ to compute finishing times :f for each vertex 
+2 as each vertex is finished, insert it onto the front of a linked list
+3 return the linked list of vertices
